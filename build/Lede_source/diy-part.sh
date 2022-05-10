73b33f67380faccdeaad4da4d3184896130d6bf8
0118Add <60157610+0118Add@users.noreply.github.com>
2022-05-10 22:55:34 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -35,7 +35,7 @@ rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-netdata
 git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-svn co https://github.com/kiddin9/openwrt-packages/trunk/netdata package/netdata
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/netdata package/netdata
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata package/luci-app-netdata
 
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
